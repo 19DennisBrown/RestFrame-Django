4b5b7af8cea8cc462a9afac41a4c9671ab93dd18
19DennisBrown <densmoney12@gmail.com>
2024-03-10 07:12:38 +0300

    Added GET and PUT methods

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+  name = models.TextField(max_length = 20)
+  country = models.TextField(max_length = 20)
+  language = models.TextField(max_length = 20)
+  province = models.TextField(max_length = 20)
+  data_date = models.DateTimeField(auto_now_add = True)
+  
+  def __str__(self):
+    return self.name
--- base/views.py
+++ base/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Person
+from .serializers import PersonSerializer
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(['GET', 'POST'])
+def view(request):
+  if request.method == 'GET':
+    persons = Person.objects.all()
+    serialized = PersonSerializer(persons, many=True)
+    return JsonResponse(serialized.data, safe=False)
+  
+  if request.method == 'POST':
+    serialized = PersonSerializer(data = request.data)
+    if serialized.is_valid():
+      serialized.save()
+      return Response(serialized.data, status = status.HTTP_201_CREATED)
