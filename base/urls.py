6f981ab2ba4b432b6044ee958082aeefeb7d9a24
19DennisBrown <densmoney12@gmail.com>
2024-03-11 10:27:17 +0300

    Added PUT and DELETE methods

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
   path('', views.view, name="view"),
+  path('person/<int:id>/', views.personView, name="person"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,12 @@
 
+
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Person
 from .serializers import PersonSerializer
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 @api_view(['GET', 'POST'])
@@ -16,7 +17,32 @@ def view(request):
     return JsonResponse(serialized.data, safe=False)
   
   if request.method == 'POST':
-    serialized = PersonSerializer(data = request.data)
+    serialized = PersonSerializer(data=request.data)
     if serialized.is_valid():
       serialized.save()
-      return Response(serialized.data, status = status.HTTP_201_CREATED)
+      return Response(serialized.data, status=status.HTTP_201_CREATED)
+  return JsonResponse(serialized.data, safe=False)
+
+
+def personView(request, id):
+
+  try:
+    person = Person.objects.get(id=id)
+  except person.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    serialized = PersonSerializer(person)
+    return JsonResponse(serialized.data, safe=False)
+  
+  if request.method == 'PUT':
+    serialized = PersonSerializer(person, data=request.data)
+    if serialized.is_valid():
+      serialized.save()
+      return Response(serialized.data)
+    else:
+      return Response(status=status.HTTP_400_BAD_REQUEST)
+
+  if request.method == 'DELETE':
+    person.delete()
+    return Response(status=status.HTTP_404_NOT_FOUND)
